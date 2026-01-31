fb3233170141b01f4dd17f70c9eb10dd690c16ac
Florian Benedetti <f-benedetti@live.com>
2026-01-31 16:04:41 +0000

    Update for rootless update

--- install_quads.sh
+++ install_quads.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "Install rootless quadlets."
+   # Destination directory for quadlet files
+   DEST_DIR="${HOME}/.config/containers/systemd/"
+   SYSTEMD_ARG="--user"
+else
+   echo "Install root quadlets."
+   # Destination directory for quadlet files
+   DEST_DIR="/etc/containers/systemd"
+   SYSTEMD_ARG=""
+fi
+
 # Directory containing the quadlet files
 QUADLET_DIR=$(pwd)
-
-# Destination directory for quadlet files
-DEST_DIR="/etc/containers/systemd"
 
 # Ensure the destination directory exists
 if [ ! -d "$DEST_DIR" ]; then
     echo "Creating destination directory: $DEST_DIR"
-    sudo mkdir -p "$DEST_DIR"
+    mkdir -p "$DEST_DIR"
 fi
 
-# Copy quadlet files to the destination directory
+# # Copy quadlet files to the destination directory
 echo "Installing quadlet files from $QUADLET_DIR to $DEST_DIR"
-sudo cp "$QUADLET_DIR"/*.container "$DEST_DIR" 2>/dev/null
-sudo cp "$QUADLET_DIR"/*.volume "$DEST_DIR" 2>/dev/null
-sudo cp "$QUADLET_DIR"/*.network "$DEST_DIR" 2>/dev/null
-sudo cp "$QUADLET_DIR"/*.env "$DEST_DIR"/env 2>/dev/null
+cp "$QUADLET_DIR"/*.container "$DEST_DIR" 2>/dev/null
+# cp "$QUADLET_DIR"/*.volume "$DEST_DIR" 2>/dev/null
+# cp "$QUADLET_DIR"/*.network "$DEST_DIR" 2>/dev/null
+# cp "$QUADLET_DIR"/*.env "$DEST_DIR"/env 2>/dev/null
 
-# Reload systemd to apply changes
+# # Reload systemd to apply changes
 echo "Reloading systemd daemon"
-sudo systemctl daemon-reload
+systemctl ${SYSTEMD_ARG} daemon-reload
 
-echo "Quadlet files installed successfully."
+# echo "Quadlet files installed successfully."
